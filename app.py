cc60806ac062e1c5d10840eff1f3cd7d304be67e
Joshua-sch <joshua@inmansol.com>
2026-07-13 09:58:21 -0400

    hotel name

--- app.py
+++ app.py
@@ -1563,7 +1563,7 @@ MULTI_ID_PREFIX = "MULTI:"
 KNOWN_MULTI_FOLDER_HOTELS = {
     "Hyannis Anchor In":     ["ANCHOR"],
     "Provincetown Surfside": ["SURFSIDE"],
-    "Hotel 1620":            ["1620"],
+    "Hotel 1620":            ["1620", "PLYMOUTH"],
 }
 
 
